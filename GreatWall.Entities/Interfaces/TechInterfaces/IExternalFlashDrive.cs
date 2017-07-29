f5047dafdd328e352298060b2418d66bb2a238d6
Georgi <georgihorozov64@gmail.com>
2017-07-29 20:53:21 +0300

    Add

--- GreatWall.Entities/Interfaces/TechInterfaces/IExternalFlashDrive.cs
+++ GreatWall.Entities/Interfaces/TechInterfaces/IExternalFlashDrive.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GreatWall.Entities.Interfaces.TechInterfaces
+﻿namespace GreatWall.Entities.Interfaces.TechInterfaces
 {
     public interface IExternalFlashDrive
     {
